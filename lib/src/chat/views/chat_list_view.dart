c1badee8e7b3907b36922c1a0a68e2bc5ebd6c5e
ashishacharya083 <imashish083@gmail.com>
2023-04-30 16:18:27 +0545

    err in androidmanifest

--- lib/src/chat/views/chat_list_view.dart
+++ lib/src/chat/views/chat_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kommunicate_flutter/kommunicate_flutter.dart';
 import 'package:raam_dhulaai/src/chat/data_source/chat_data_source.dart';
 import 'package:raam_dhulaai/src/chat/providers/chat_list_provider.dart';
 import 'package:raam_dhulaai/src/core/extensions/context_extension.dart';
@@ -45,23 +47,45 @@ class _ChatListViewState extends ConsumerState<ChatListView> {
       body: ref.watch(chatListNotifierProvider(isMerchant)).maybeWhen(
             orElse: Container.new,
             loading: () => context.loader,
-            success: (data) => ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                final room = data[index];
-                return ChatListTile(
-                  imageUrl: '',
-                  name: room.product?.name ?? '',
-                  subtitle: isMerchant ? (room.user?.name ?? '') : (room.merchant?.name ?? ''),
-                  onPressed: () => context.router.push(ChatDetailRoute(
-                    product: room.product!,
-                    roomId: room.id,
-                    isMerchant: isMerchant,
-                    merchant: room.merchant,
-                    user: room.user,
-                  )),
-                );
-              },
+            success: (data) => Column(
+              children: [
+                GestureDetector(
+                  onTap: ()async{
+                    try {
+     dynamic conversationObject = {
+         'appId': dotenv.env['KOMMUNICATE_APPID'] 
+     };
+      dynamic result = await KommunicateFlutterPlugin.buildConversation(conversationObject);
+      print("Conversation builder success : " + result.toString());
+    } on Exception catch (e) {
+      print("Conversation builder error occurred : " + e.toString());
+    }
+                  },
+                  child: Container(
+                    child: Text('Chat'),
+                  ),
+                ),
+                ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    final room = data[index];
+                    return ChatListTile(
+                      imageUrl: '',
+                      name: room.product?.name ?? '',
+                      subtitle: isMerchant
+                          ? (room.user?.name ?? '')
+                          : (room.merchant?.name ?? ''),
+                      onPressed: () => context.router.push(ChatDetailRoute(
+                        product: room.product!,
+                        roomId: room.id,
+                        isMerchant: isMerchant,
+                        merchant: room.merchant,
+                        user: room.user,
+                      )),
+                    );
+                  },
+                ),
+              ],
             ),
           ),
     );
