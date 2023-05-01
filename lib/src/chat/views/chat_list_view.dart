191e284c72f84796c74dfda3a4b7013330241413
ashishacharya083 <imashish083@gmail.com>
2023-05-01 14:12:09 +0545

    add chat bot

--- lib/src/chat/views/chat_list_view.dart
+++ lib/src/chat/views/chat_list_view.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:raam_dhulaai/src/chat/data_source/chat_data_source.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kommunicate_flutter/kommunicate_flutter.dart';
 import 'package:raam_dhulaai/src/chat/providers/chat_list_provider.dart';
 import 'package:raam_dhulaai/src/core/extensions/context_extension.dart';
-import 'package:raam_dhulaai/src/core/providers/firbease_provider.dart';
 import 'package:raam_dhulaai/src/core/routes/app_router.dart';
 import 'package:raam_dhulaai/src/core/widgets/widgets.dart';
 import 'package:raam_dhulaai/src/home/widgets/widgets.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ChatListView extends StatefulHookConsumerWidget {
   const ChatListView({Key? key, required this.isMerchant}) : super(key: key);
@@ -42,6 +42,20 @@ class _ChatListViewState extends ConsumerState<ChatListView> {
       //       },
       //     ),
       // body: Column(),
+      floatingActionButton: GestureDetector(
+        onTap: () async {
+          try {
+            dynamic conversationObject = {'appId': dotenv.env['KOMMUNICATE_APPID']};
+            dynamic result = await KommunicateFlutterPlugin.buildConversation(conversationObject);
+            print("Conversation builder success : " + result.toString());
+          } on Exception catch (e) {
+            print("Conversation builder error occurred : " + e.toString());
+          }
+        },
+        child: CircleAvatar(
+          child: Icon(Icons.chat),
+        ),
+      ),
       body: ref.watch(chatListNotifierProvider(isMerchant)).maybeWhen(
             orElse: Container.new,
             loading: () => context.loader,
@@ -68,24 +82,23 @@ class _ChatListViewState extends ConsumerState<ChatListView> {
   }
 }
 
-
- // ref.watch(getChatListProvider(widget.isMerchant)).maybeWhen(
-          //       orElse: Container.new,
-          //       data: (data) => SliverList(
-          //         delegate: SliverChildBuilderDelegate(
-          //           (context, index) => StreamBuilder(
-          //               stream: ref.watch(getChatListProvider(true).stream),
-          //               builder: (context, value) {
-          //                 print(value);
-          //                 return ChatListTile(
-          //                   onPressed: () {
-          //                     context.router.push(const ChatDetailRoute());
-          //                   },
-          //                   imageUrl: 'https://placehold.jp/30/dd6699/ffffff/300x150.png?text=A',
-          //                   name: 'Aayush Bhattarai',
-          //                   subtitle: 'Hi, I just booked service.',
-          //                 );
-          //               }),
-          //         ),
-          //       ),
-          //     ),
+// ref.watch(getChatListProvider(widget.isMerchant)).maybeWhen(
+//       orElse: Container.new,
+//       data: (data) => SliverList(
+//         delegate: SliverChildBuilderDelegate(
+//           (context, index) => StreamBuilder(
+//               stream: ref.watch(getChatListProvider(true).stream),
+//               builder: (context, value) {
+//                 print(value);
+//                 return ChatListTile(
+//                   onPressed: () {
+//                     context.router.push(const ChatDetailRoute());
+//                   },
+//                   imageUrl: 'https://placehold.jp/30/dd6699/ffffff/300x150.png?text=A',
+//                   name: 'Aayush Bhattarai',
+//                   subtitle: 'Hi, I just booked service.',
+//                 );
+//               }),
+//         ),
+//       ),
+//     ),
