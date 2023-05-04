93d74a037de8134a66bc279ff7b49279714177a9
ashishacharya083 <imashish083@gmail.com>
2023-05-04 22:16:25 +0545

    add cloth recognition view

---
+++ lib/src/cloth_recognition/cloth_recognition_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class ClothRecognitionView extends ConsumerStatefulWidget {
+  const ClothRecognitionView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  ConsumerState createState() => _ClothRecognitionViewState();
+}
+
+class _ClothRecognitionViewState extends ConsumerState<ClothRecognitionView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
